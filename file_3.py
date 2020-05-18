116989a6d479d6e14a0e611df62082c6121c6cc1
joppe <joppe.vos@gmail.com>
2020-05-18 18:47:30 +0200

    extra video file

---
+++ file_3.py
@@ -0,0 +1,32 @@
+b = {"bigbang":"c6fd87b93a1999da820f",
+ "cell_life": "1c077cfd7a5da3913fb7",
+ "character_dark": "0375a6918b5b2124e4ba",
+ "water_beast": "0849e32c000c9cc66094",
+ "jelly_light": "e39fe909fa12bee02186",
+ "jellyfish" : "18529dfff594c65d3e77",
+ "jelly_group_blue": "3478672ec63543300afc",
+ "vulcano_erupt": "fb985b00df8815bd8542",
+ "mountains_blue": "39db8a38d21223733f45",
+ "mountain_church": "42ee7bbb27a0d26f706f",
+ "temple": "347bb0059d3bdd0bb02a1794",
+ "gold_fractal" : "e588bb5cb18e75c257db",
+ "character_two_guys": "3603a92cfe03756da2a0",
+ "dark_church": "8d451fdac32f9698c012",
+ "civilians": "1078f5d7b2ed017cc4bd",
+ "industrial_blackwhite" : "69b217dbac60e2726019",
+ "industrial_40e" : "3f64c46859702a7093c1",
+ "cyberpunk_people_red": "12b0edd467e039c75732",
+ "character": "85cb95b0cba17d6e9415",
+ "future_bedroom" : "6a97704757881f4d81e7",
+ "to_industrial" : "3b598df7432e3a37d53e",
+ "launch_abstract": "7056e27fdd265d327a9f",
+ "character_end": "98e22dbdbbb137c27ef9",
+ "flame_burn_out": "b6bba6b54746cdf33b22",
+# "black" : "1446bfb10608a94d811d"
+ }
+
+def breeder(breeder):
+    for k,v in breeder.items():
+        print(v)
+
+breeder(b)
